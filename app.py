05f1423e15666f856c8ff2eef8c06a85ce0fff11
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2022-07-08 01:37:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ using neural networks. How does the application work? The user has the option to
 model then tries to create a label that is as accurate as possible. To evaluate, the user can vote via a button which 
 of the two captions they think better represents the image content. The results are displayed in a graph for viewing 
 and further analysis. 
-(Note: The voting is currently still a proof of concept and is not fully functional!""")
+(Note: The voting is currently still a proof of concept and is not fully functional!)""")
 st.write("")
 
 # Data Set
