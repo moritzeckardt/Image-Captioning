39e648d4e00f36f00219e0e88444013259f00a32
Moritz Eckardt <83656997+moritzeckardt@users.noreply.github.com>
2022-07-08 08:53:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,9 @@ st.write("# ML4B - Image Caption Generator")
 st.header("Short project explanation")
 st.write("""Hello, we are Jonas, Moritz and Ole. Together we want to understand, build and present a caption generator 
 using neural networks. How does the application work? The user has the option to either upload an image or select one from the dataset. Our 
-model then tries to create a label that is as accurate as possible. To evaluate, the user can vote via a button which 
-of the two captions they think better represents the image content. The results are displayed in a graph for viewing 
-and further analysis. 
-(Note: The voting is currently still a proof of concept and is not fully functional!)""")
+model then tries to create a caption that is as accurate as possible. To evaluate, the user can vote via a button which 
+if the generated caption represents the image content precisely. The results are displayed in a graph for viewing 
+and further analysis (Note: The voting is currently still a proof of concept and is not fully functional!).""")
 st.write("")
 
 # Data Set
