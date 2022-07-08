fd3b97998b30638c18e35bce850ffb7871ca64c6
Moritz Eckardt <83656997+moritzeckardt@users.noreply.github.com>
2022-07-08 08:55:24 +0200

    Update Business_Understanding.py

--- pages/Business_Understanding.py
+++ pages/Business_Understanding.py
@@ -8,15 +8,13 @@ st.markdown("""<p style='text-align: center'>While many teams hurry through this
 with st.expander("Motivation"):
     st.write("""What was our motivation? We are excited about machine learning in the context of visual challenges, for example, autonomous driving, which can 
              provide more safety on the road. For this reason, we were eager to develop an application in the field of labeling. Through the project, we hoped to be 
-             able to practically apply our theoretical foundations and expand our knowledge in the field of screen recognition. and to expand our knowledge in the 
-             field of image recognition and labeling.""")
+             able to practically apply our theoretical foundations and expand our knowledge in the field of image recognition and labeling.""")
 
 with st.expander("Determining Business Objectives"):
-    st.write("""At the beginning, we had many ideas that usually had to be changed during the project due to factors such as time, feasibility and difficulty,
-             Feasibility and difficulty. The goal, of course, was to train a model that could generate subtitles as accurately as possible.
-             Finding a model that had already been trained, as well as fine-tuning and transferring it to the Streamlit application, proved in some cases to be
-             more difficult than expected in some cases. Nonetheless, we can now present a relatively good model that can 
-             present some features to engage the user in the application.""")
+    st.write("""At the beginning, we had many ideas that usually had to be changed during the project due to factors such as time, feasibility and difficulty. 
+             The goal, of course, was to train a model that could generate subtitles as accurately as possible. Finding a model, as well as training and transferring 
+             it to the Streamlit application, proved in some cases to be more difficult than expected in some cases. Nonetheless, we can now present a relatively good 
+             model that can present some features to engage the user in the application.""")
 
 with st.expander("Assess Situation"):
     st.write("""Of course, we thought about what resources we would need and what problems we might encounter beforehand.
@@ -26,8 +24,8 @@ with st.expander("Assess Situation"):
 
 with st.expander("Determine Data Mining Goals"):
     st.write("""We did not set ourselves a specific technical goal. First of all, it was important for us to develop an understanding
-             and thus to have a model that can roughly and comprehensibly recognize the image content. Based on this, we definitely
-             we definitely wanted to include the user as well as some features.""")
+             and thus to have a model that can roughly and comprehensibly recognize the image content. Based on this, we definitely wanted to include 
+             the user as well as some features.""")
 
 with st.expander("Produce Project Plan"):
     st.write("""We designed our project plan to be relatively flexible. We had set ourselves specific deadlines early on, divided the tasks among ourselves accordingly,
